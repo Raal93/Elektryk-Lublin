84cad0fd1986507e47389a735805ef063138794b
Rafał Czajkowski <Raal93@gmail.com>
2020-11-06 19:21:42 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,34 @@
 
     $('.mobile-menu').on('click', function() {
         $('.primary-menu ul').slideToggle();
-    })
+    });
 
-}) (jQuery)
+
+    $(document).ready(function() {
+        $(".owl-carousel").owlCarousel({
+            center: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true,
+            autoplaySpeed: 1500,
+            items: 2,
+            loop: true,
+            dots: false,
+            margin: 20,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                	items: 2
+                },
+                992: {
+                	items: 3
+                }
+            }
+        });
+    });
+
+
+
+})(jQuery)
